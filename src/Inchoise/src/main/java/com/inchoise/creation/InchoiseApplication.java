ddd257d12ff0a1bac63bc478b60fe6104a39af75
Play9555 <gajulapavanhruthik33@gmail.com>
2024-06-16 00:38:31 +0530

    Base application created

---
+++ src/Inchoise/src/main/java/com/inchoise/creation/InchoiseApplication.java
@@ -0,0 +1,13 @@
+package com.inchoise.creation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InchoiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InchoiseApplication.class, args);
+	}
+
+}
---
+++ src/Inchoise/src/test/java/com/inchoise/creation/InchoiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inchoise.creation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InchoiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
